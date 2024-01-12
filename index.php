ed6897ab1da7ca52383f9e3cbe150e5d97a093c3
milanpatel1822 <bhimanimilan13@gmail.com>
2024-01-12 09:47:11 +0530

    remening-code

--- index.php
+++ index.php
@@ -3,76 +3,6 @@ include('header.php');
 ?>
 <div class="overLay"></div>
 
-
-<section class="background firstsection">
-    <div class="box-main">
-        <div class="firsthalf">
-            <p class="text-big">
-                Your <I>Dream </I> House
-            </p>
-            <p class="text-small">
-                It is tangible,it's solid ,it's beautiful.It is artistic,<br>from my standpoint, and I just love
-                real estate.
-            </p>
-            <div class="buttons">
-                <button class="btn" onclick="showModal()">LogIn</button>
-            </div>
-        </div>
-
-        <div class="secondhalf">
-            <img src="img/bg.jpg" alt="laptop">
-        </div>
-    </div>
-
-</section>
-
-<div class="card-head" value="Service">
-    <div class="heading option-heading">
-        <h3>Sell & Rent House</h3>
-    </div>
-    <div class="card-container">
-        <div class="card-item">
-            <img src="img/bg.jpg" alt="">
-            <p class="card-text">Loremipsum.</p>
-        </div>
-        <div class="card-item">
-            <img src="img/bg.jpg" alt="">
-        </div>
-        <div class="card-item">
-            <img src="img/bg.jpg" alt="">
-        </div>
-        <div class="card-item">
-            <img src="img/bg.jpg" alt="">
-        </div>
-    </div>
-</div>
-
-<div class="card-shops">
-    <div class="heading option-heading">
-        <h3>Shops Suitable for your Business</h3>
-    </div>
-    <div class="card-shop-container">
-        <div class="card-shop-item">
-            <img src="img/bg.jpg" alt="images">
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatibus, blanditiis?</p>
-        </div>
-    </div>
-    <div class="card-shop-container">
-        <div class="card-shop-item">
-            <img src="img/bg.jpg" alt="images">
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatibus, blanditiis?</p>
-        </div>
-    </div>
-    <div class="card-shop-container">
-        <div class="card-shop-item">
-            <img src="img/bg.jpg" alt="images">
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatibus, blanditiis?</p>
-        </div>
-    </div>
-
-
-</div>
-
 <section>
     <div class="form-structor">
         <span class="cancle" onclick="removeModal()">X </span>
@@ -99,10 +29,28 @@ include('header.php');
     </div>
 </section>
 
+<div class="card-head" value="Service">
+    <div class="heading option-heading">
+        <h3>Sell & Rent House</h3>
+    </div>
+    <div class="card-container">
+        <div class="card-item">
+            <img src="img/bg.jpg" alt="">
+            <p class="card-text">Loremipsum.</p>
+        </div>
+        <div class="card-item">
+            <img src="img/bg.jpg" alt="">
+        </div>
+        <div class="card-item">
+            <img src="img/bg.jpg" alt="">
+        </div>
+        <div class="card-item">
+            <img src="img/bg.jpg" alt="">
+        </div>
+    </div>
+</div> 
 
+<?php
+include('footer.php');
+?>
 
-<script src="js/script.js"></script>
-
-</body>
-
-</html>
